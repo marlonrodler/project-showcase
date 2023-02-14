e62a9b2420a4323f7ee5eb2578b9fc5e6c56e54a
Marlon Rodler <marlon.rodler@hotmail.com>
2023-02-14 11:00:14 -0300

    dev: ajustes finais

--- src/pages/Showcase/index.js
+++ src/pages/Showcase/index.js
@@ -1,37 +1,34 @@
 import React, { useState, useEffect, useRef } from "react";
-import { VscDebugStart } from "react-icons/vsc";
+import { BsGithub, BsFillPlayCircleFill } from "react-icons/bs";
+import myProjects from "../../data/myProjects";
 
 import "./styles.css";
 
 export default function Showcase() {
-  const [projects, setProjects] = useState([
-    'https://raw.githubusercontent.com/marlonrodler/typing-speed/main/typing-speed.jpg', 
-    'https://raw.githubusercontent.com/marlonrodler/pokedex-react/main/pokedex-react.jpg',
-    'https://raw.githubusercontent.com/marlonrodler/clone-spotify/master/clone-spotify.jpg'
-  ]);
+  const [projects, setProjects] = useState(myProjects);
   const refCard = useRef(null);
 
   useEffect(() => {
-    console.log('refCard:',refCard);
 
-    setTimeout(() => {
-      for (let i = 0; i < projects.length; i++) {
-        refCard[i].style.backgroundImage = `url(${projects[i]})`;
-      }
-    }, 1000);
-    
+    for (const key of Object.keys(projects)) {
+      refCard[key].style.backgroundImage = `url(${projects[key][0]})`;
+    }
   }, [projects]);
-
 
   return (
     <div className="container">
       <div className="cards">
         {
-          projects.map((item, index) => {
+
+          Object.entries(projects).map(([key, value]) => {
             return (
-              <div ref={(span) => { refCard[index] = span }} className='card' key={index}>
-                <div className="card-content"></div>
-                <div className="card-content"></div>
+              <div ref={(span) => { refCard[key] = span }} className='card' key={key}>
+                <a className="card-content" href={value[1]} target='_blank' rel="noreferrer">
+                  <BsGithub />
+                </a>
+                <a className="card-content" href={value[2]} target='_blank' rel="noreferrer">
+                  <BsFillPlayCircleFill />
+                </a>
               </div>
             )
           })
